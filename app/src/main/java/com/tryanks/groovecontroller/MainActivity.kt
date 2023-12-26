5fb79f8d0c3328ef3a07e648a3ffd74d580d9c8c
Tryanks <tryanks@outlook.com>
2023-12-26 08:02:57 +0800

    Add Bluetooth scan Impl

--- app/src/main/java/com/tryanks/groovecontroller/MainActivity.kt
+++ app/src/main/java/com/tryanks/groovecontroller/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tryanks.groovecontroller
 
+import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothHidDevice
@@ -13,19 +14,27 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
-    private val btAdapter: BluetoothAdapter? = bluetoothAdapter()
+    private var btAdapter: BluetoothAdapter? = null
     private var hidDevice: BluetoothHidDevice? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        ActivityCompat.requestPermissions(this, arrayOf(
+            android.Manifest.permission.BLUETOOTH,
+            android.Manifest.permission.BLUETOOTH_CONNECT,
+            android.Manifest.permission.BLUETOOTH_ADVERTISE,
+        ), 0)
+
+        btAdapter = bluetoothAdapter()
         callBluetooth()
         if (hidDevice == null) return
 
@@ -34,17 +43,14 @@ class MainActivity : AppCompatActivity() {
     private fun callBluetooth() {
         if (btAdapter == null) return
         val ctx = this
-        btAdapter.getProfileProxy(this, object : BluetoothProfile.ServiceListener {
+        btAdapter!!.getProfileProxy(this, object : BluetoothProfile.ServiceListener {
             @RequiresApi(Build.VERSION_CODES.S)
             override fun onServiceConnected(profile: Int, proxy: BluetoothProfile?) {
-                val check = checkPermission()
-                if (check.isNotEmpty()) {
-                    ActivityCompat.requestPermissions(ctx, check.split(" ").toTypedArray(), 0)
-                }
+                checkPermission()
                 if (profile == BluetoothProfile.HID_DEVICE) {
                     hidDevice = proxy as BluetoothHidDevice
                 }
-                registerBluetoothHid()
+                registerBluetoothHid(KeyboardReport())
                 startActivityForResult(Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE), 0)
             }
 
@@ -59,28 +65,21 @@ class MainActivity : AppCompatActivity() {
     private var hostDevice: BluetoothDevice? = null
 
     @RequiresApi(Build.VERSION_CODES.S)
-    private fun registerBluetoothHid() {
+    private fun registerBluetoothHid(report: BasicReport) {
         val qosSettings = BluetoothHidDeviceAppQosSettings(
             BluetoothHidDeviceAppQosSettings.SERVICE_BEST_EFFORT,
             800, 9, 0, 11250, BluetoothHidDeviceAppQosSettings.MAX
         )
         val keyboardSdpSettings = BluetoothHidDeviceAppSdpSettings(
-            KeyboardName, Description, ProviderName, BluetoothHidDevice.SUBCLASS1_KEYBOARD, hidDesc
+            report.name, report.description, report.providerName, report.subClass, report.descReport
         )
         // TODO: BluetoothHidDevice.SUBCLASS2_JOYSTICK
 
-        val check = checkPermission()
-        if (check.isNotEmpty()) {
-            ActivityCompat.requestPermissions(this, check.split(" ").toTypedArray(), 0)
-        }
-        val ctx = this
+        checkPermission()
         hidDevice!!.registerApp(keyboardSdpSettings, null, qosSettings, Executors.newCachedThreadPool(), object :
             BluetoothHidDevice.Callback() {
                 override fun onAppStatusChanged(pluggedDevice: BluetoothDevice?, registered: Boolean) {
-                    val check = checkPermission()
-                    if (check.isNotEmpty()) {
-                        ActivityCompat.requestPermissions(ctx, check.split(" ").toTypedArray(), 0)
-                    }
+                    checkPermission()
                     if (registered) {
                         if (pluggedDevice != null && hidDevice!!.getConnectionState(pluggedDevice) != BluetoothProfile.STATE_CONNECTED) {
                             val result = hidDevice!!.connect(pluggedDevice)
@@ -101,23 +100,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     @RequiresApi(Build.VERSION_CODES.S)
-    private fun checkPermission(): String {
-        var permissionMessage = ""
-        arrayOf(
-            android.Manifest.permission.BLUETOOTH,
-            android.Manifest.permission.BLUETOOTH_CONNECT,
-            android.Manifest.permission.BLUETOOTH_ADVERTISE,
-        ).forEach {
-            if (ActivityCompat.checkSelfPermission(this, it)
-                != PackageManager.PERMISSION_GRANTED
-            ) {
-                permissionMessage += "$it "
-            }
+    private fun checkPermission() {
 
-        }
-        return permissionMessage
     }
 }
 
 fun Context.bluetoothAdapter(): BluetoothAdapter? =
     (this.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).adapter
+
+class HidDeviceCallBack: BluetoothHidDevice.Callback() {
+    val btHidDevice: BluetoothHidDevice? = null
+    override fun onAppStatusChanged(pluggedDevice: BluetoothDevice?, registered: Boolean) {
+        if (registered) {
+            Log.d("HidDeviceCallBack", "registered$pluggedDevice")
+        } else {
+            Log.d("HidDeviceCallBack", "unregistered$pluggedDevice")
+        }
+    }
+
+    override fun onGetReport(device: BluetoothDevice?, type: Byte, id: Byte, bufferSize: Int) {
+//        btHidDevice.sendReport(device, type, id, hidReport)
+    }
+
+    override fun onConnectionStateChanged(device: BluetoothDevice?, state: Int) {
+        super.onConnectionStateChanged(device, state)
+    }
+}
---
+++ app/src/main/java/com/tryanks/groovecontroller/Reports.kt
@@ -0,0 +1,104 @@
+package com.tryanks.groovecontroller
+
+import android.bluetooth.BluetoothHidDevice
+
+enum class ControlEvent(val value: Int) {
+    LZoneTap(0),
+    LZoneUp(1),
+    LZoneDown(2),
+    LZoneLeft(3),
+    LZoneRight(4),
+
+    RZoneTap(5),
+    RZoneUp(6),
+    RZoneDown(7),
+    RZoneLeft(8),
+    RZoneRight(9),
+    ;
+}
+
+interface BasicReport {
+    val name: String
+    val description: String
+    val providerName: String
+    val descReport: ByteArray
+    val subClass: Byte
+//    fun SendEvent(e: ControlEvent)
+}
+
+class KeyboardReport: BasicReport {
+    override val name = "Groove Coaster Keyboard"
+    override val description = "A Controller emulator for Groove Coaster"
+    override val providerName = "Tryanks"
+    override val subClass: Byte = BluetoothHidDevice.SUBCLASS1_KEYBOARD
+    override val descReport: ByteArray = intArrayOf(
+        0x05, 0x01, // Usage Page (Generic Desktop)
+        0x09, 0x06, // Usage (Keyboard)
+        0xA1, 0x01, // Collection (Application)
+        0x05, 0x07, //   Usage Page (Key Codes)
+        0x19, 0xE0, //   Usage Minimum (224)
+        0x29, 0xE7, //   Usage Maximum (231)
+        0x15, 0x00, //   Logical Minimum (0)
+        0x25, 0x01, //   Logical Maximum (1)
+        0x75, 0x01, //   Report Size (1)
+        0x95, 0x08, //   Report Count (8)
+        0x81, 0x02, //   Input (Data, Variable, Absolute): Modifier byte
+        0x75, 0x08, //   Report Size (8)
+        0x95, 0x01, //   Report Count (1)
+        0x81, 0x01, //   Input (Constant): Reserved byte
+        0x05, 0x08, //   Usage Page (LEDs)
+        0x19, 0x01, //   Usage Minimum (1)
+        0x29, 0x05, //   Usage Maximum (5)
+        0x75, 0x01, //   Report Size (1)
+        0x95, 0x05, //   Report Count (5)
+        0x91, 0x02, //   Output (Data, Variable, Absolute): LED report
+        0x75, 0x03, //   Report Size (3)
+        0x95, 0x01, //   Report Count (1)
+        0x91, 0x01, //   Output (Constant): LED report padding
+        0x05, 0x07, //   Usage Page (Key Codes)
+        0x19, 0x00, //   Usage Minimum (0)
+        0x29, 102 - 1, //   Usage Maximum (101)
+        0x15, 0x00, //   Logical Minimum (0)
+        0x25, 102 - 1, //   Logical Maximum(101)
+        0x75, 0x08, //   Report Size (8)
+        0x95, 6, //   Report Count (6)
+        0x81, 0x00, //   Input (Data, Array): Keys
+        0xC0, //       End Collection
+    ).map { it.toByte() }.toByteArray()
+}
+
+class JoystickReport: BasicReport {
+    override val name = "Groove Coaster Joystick"
+    override val description = "A Controller emulator for Groove Coaster"
+    override val providerName = "Tryanks"
+    override val subClass: Byte = BluetoothHidDevice.SUBCLASS2_JOYSTICK
+    override val descReport: ByteArray = intArrayOf(
+        0x05, 0x01, // Usage Page (Generic Desktop)
+        0x09, 0x04, // Usage (Joystick)
+        0xA1, 0x01, // Collection (Application)
+        0x85, 0x01, //   Report ID (1)
+        0x05, 0x01, //   Usage Page (Generic Desktop)
+        0x09, 0x30, //   Usage (X)
+        0x09, 0x31, //   Usage (Y)
+        0x09, 0x32, //   Usage (Z)
+        0x09, 0x35, //   Usage (Rz)
+        0x15, 0x00, //   Logical Minimum (0)
+        0x26, 0xFF, 0x00, //   Logical Maximum (255)
+        0x75, 0x08, //   Report Size (8)
+        0x95, 0x04, //   Report Count (4)
+        0x81, 0x02, //   Input (Data, Variable, Absolute)
+        0x05, 0x09, //   Usage Page (Button)
+        0x19, 0x01, //   Usage Minimum (1)
+        0x29, 0x0E, //   Usage Maximum (14)
+        0x15, 0x00, //   Logical Minimum (0)
+        0x25, 0x01, //   Logical Maximum (1)
+        0x75, 0x01, //   Report Size (1)
+        0x95, 0x0E, //   Report Count (14)
+        0x81, 0x02, //   Input (Data, Variable, Absolute)
+        0x05, 0x01, //   Usage Page (Generic Desktop)
+        0x09, 0x39, //   Usage (Hat switch)
+        0x15, 0x00, //   Logical Minimum (0)
+        0x25, 0x07, //   Logical Maximum (7)
+        0x35, 0x00, //
+    ).map { it.toByte() }.toByteArray()
+}
